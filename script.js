02f4fc3e35af17adc9a79abd3c1ce6a942e80662
Robert McLeod <hamstar@telescum.co.nz>
2012-09-24 22:39:45 +1200

    made the script hide title and add buttons

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+autobackup = {
+	
+	hide_title: function() {
+		jQuery("div.page h1.title").remove();
+	},
+
+	add_memories_button: function() {
+		li = jQuery("<li/>").append( 
+			jQuery("<a/>")
+			.addClass("action")
+			.attr("href", "/doku.php?id=test:memories&do=memories")
+			.text("Memories")
+		);
+
+		jQuery("div#dokuwiki__usertools ul").prepend( li );
+	}
+};
+
 jQuery(document).ready(function() {
+
+	// Modify the page
+	autobackup.hide_title();
+	autobackup.add_memories_button();
 
 	jQuery("#Disable_dropbox").click(function() {
 		jQuery.post(
